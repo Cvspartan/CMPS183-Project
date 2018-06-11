bfaebe62f9d07905ae08bddb8f2ebc38bc5736b2
Chris Vu <chrisacc123@yahoo.com>
2018-06-11 00:45:44 -0700

    better html

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$a9876cab08f7d69e$3692ebef89ab5778a5bdcc1f79b8f8c27521ef08"
+password="pbkdf2(1000,20,sha512)$8f09022b51900ad7$60c68ed3dec61e925b27b5cfe159cba72ccc5848"
