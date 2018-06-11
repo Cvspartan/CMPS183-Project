6258c5663b228a76cba9d30dbe0845534ae4ae42
Chris Vu <chrisacc123@yahoo.com>
2018-06-11 16:30:47 -0700

    fixed merge conflicts

--- web2py/parameters_8000.py
+++ web2py/parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$80b46f677cea7657$78aedefefb4f37c61d26ffa64e0fa99dd8ff90e0"
+password="pbkdf2(1000,20,sha512)$87a6995fd455f1b7$a9d6cb7a92b96eb8f6d2dc5a9ca2a838e47a1ac1"
